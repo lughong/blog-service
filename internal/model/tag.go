b593fd59d04e0854d480d3d7ba24645a2ec98f87
lugh <1586668924@qq.com>
2020-08-17 22:44:10 +0800

    add tag model

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -1,6 +1,10 @@
 package model
 
-import "github.com/lughong/blog-service/pkg/app"
+import (
+	"github.com/jinzhu/gorm"
+
+	"github.com/lughong/blog-service/pkg/app"
+)
 
 type TagSwagger struct {
 	List  []*Tag
@@ -16,4 +20,49 @@ type Tag struct {
 
 func (t Tag) TableName() string {
 	return "tag"
+}
+
+func (t Tag) Count(db *gorm.DB) (int, error) {
+	var count int
+	if t.Name != "" {
+		db = db.Where("name=?", t.Name)
+	}
+
+	db = db.Where("state=?", t.State)
+	if err := db.Model(&t).Where("is_del=?", 0).Count(&count).Error; err != nil {
+		return 0, err
+	}
+
+	return count, nil
+}
+
+func (t Tag) List(db *gorm.DB, pageOffset int, pageSize int) ([]*Tag, error) {
+	var tags []*Tag
+
+	if pageOffset >= 0 && pageSize > 0 {
+		db = db.Offset(pageOffset).Limit(pageSize)
+	}
+
+	if t.Name != "" {
+		db = db.Where("name=?", t.Name)
+	}
+
+	db = db.Where("state=?", t.State)
+	if err := db.Model(&t).Where("is_del=?", 0).Find(&tags).Error; err != nil {
+		return nil, err
+	}
+
+	return tags, nil
+}
+
+func (t Tag) Create(db *gorm.DB) error {
+	return db.Create(&t).Error
+}
+
+func (t Tag) Update(db *gorm.DB, values ...interface{}) error {
+	return db.Model(&t).Where("id=? AND is_del=?", t.Id, 0).Update(values).Error
+}
+
+func (t Tag) Delete(db *gorm.DB) error {
+	return db.Where("id=? AND is_del=?", t.Id, 0).Delete(&t).Error
 }
