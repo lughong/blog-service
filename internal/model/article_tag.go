db36f7795ead80ac55caee666d5277844e16803c
Your Name <you@example.com>
2020-10-14 22:43:50 +0800

    change article_tag model

--- internal/model/article_tag.go
+++ internal/model/article_tag.go
@@ -13,15 +13,50 @@ func (at ArticleTag) TableName() string {
 	return "article_tag"
 }
 
-func (at ArticleTag) Count(db *gorm.DB) (int, error)          {}
-func (at ArticleTag) Get(db *gorm.DB) (ArticleTag, error)     {}
-func (at ArticleTag) List(db *gorm.DB) ([]*ArticleTag, error) {}
+func (at ArticleTag) GetByAID(db *gorm.DB) (ArticleTag, error) {
+	var articleTag ArticleTag
+
+	err := db.Where("tag_id = ? AND is_del = ?", at.TagID, 0).First(&articleTag).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return articleTag, err
+	}
+
+	return articleTag, nil
+}
+
+func (at ArticleTag) ListByTID(db *gorm.DB) ([]*ArticleTag, error) {
+	var articleTags []*ArticleTag
+
+	if err := db.Where("tag_id = ? AND is_del = ?", at.TagID, 0).Find(&articleTags).Error; err != nil {
+		return nil, err
+	}
+
+	return articleTags, nil
+}
+
+func (at ArticleTag) ListByAIDs(db *gorm.DB, articleIDs []uint32) ([]*ArticleTag, error) {
+	var articleTags []*ArticleTag
+
+	err := db.Where("article_id IN (?) AND is_del = ?", articleIDs, 0).Find(&articleTags).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
+		return nil, err
+	}
+
+	return articleTags, nil
+}
+
 func (at ArticleTag) Create(db *gorm.DB) error {
 	return db.Create(&at).Error
 }
-func (at ArticleTag) Update(db *gorm.DB, values ...interface{}) error {
-	return db.Model(&at).Where("id=? AND is_del=?", at.ID, 0).Update(values).Error
+
+func (at ArticleTag) UpdateOne(db *gorm.DB, values ...interface{}) error {
+	return db.Model(&at).Where("article_id=? AND is_del=?", at.ArticleID, 0).Limit(1).Updates(values).Error
 }
+
 func (at ArticleTag) Delete(db *gorm.DB) error {
 	return db.Where("id=? AND is_del=?", at.ID, 0).Delete(&at).Error
+}
+
+func (at ArticleTag) DeleteOne(db *gorm.DB) error {
+	return db.Where("article_id=? AND is_del=?", at.ArticleID, 0).Limit(1).Delete(&at).Error
 }
