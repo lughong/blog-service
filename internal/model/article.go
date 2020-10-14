4d53385272e2fb4a488210a6f186dc771291da37
Your Name <you@example.com>
2020-10-14 22:31:52 +0800

    change article model

--- internal/model/article.go
+++ internal/model/article.go
@@ -10,6 +10,16 @@ type ArticleSwagger struct {
 	Pager *app.Pager
 }
 
+type ArticleRow struct {
+	ArticleID     uint32
+	TagID         uint32
+	TagName       string
+	ArticleTitle  string
+	ArticleDesc   string
+	CoverImageUrl string
+	Content       string
+}
+
 type Article struct {
 	*Model
 
@@ -27,8 +37,12 @@ func (a Article) Update(db *gorm.DB, values ...interface{}) error {
 	return db.Model(&a).Where("id=? AND is_del=?", a.ID, 0).Update(values).Error
 }
 
-func (a Article) Create(db *gorm.DB) error {
-	return db.Create(&a).Error
+func (a Article) Create(db *gorm.DB) (*Article, error) {
+	if err := db.Create(&a).Error; err != nil {
+		return nil, err
+	}
+
+	return &a, nil
 }
 
 func (a Article) Delete(db *gorm.DB) error {
@@ -38,33 +52,71 @@ func (a Article) Delete(db *gorm.DB) error {
 func (a Article) Get(db *gorm.DB) (Article, error) {
 	var article Article
 
-	if err := db.Model(&a).Where("is_del = ?", 0).First(&article).Error; err != nil {
+	db = db.Where("id = ? AND state = ? AND is_del = ?", a.ID, a.State, 0)
+	err := db.First(&article).Error
+	if err != nil && err != gorm.ErrRecordNotFound {
 		return article, err
 	}
 
 	return article, nil
 }
 
-func (a Article) List(db *gorm.DB, pageOffset, pageSize int) ([]*Article, error) {
-	var articles []*Article
+func (a Article) ListByTagID(db *gorm.DB, tagID uint32, pageOffset, pageSize int) ([]*ArticleRow, error) {
+	fields := []string{
+		"ar.id AS article_id",
+		"ar.title AS article_title",
+		"ar.desc AS article_desc",
+		"ar.cover_image_url",
+		"ar.content",
+	}
+	fields = append(fields, []string{"t.id AS tag_id", "t.name AS tag_name"}...)
 
 	if pageOffset >= 0 && pageSize > 0 {
 		db = db.Offset(pageOffset).Limit(pageSize)
 	}
 
-	db = db.Where("state = ?", a.State)
-	if err := db.Model(&a).Where("is_del = ?", 0).Find(&articles).Error; err != nil {
+	rows, err := db.Select(fields).Table(ArticleTag{}.TableName()+" AS at").
+		Joins("LEFT JOIN `"+Tag{}.TableName()+"` AS t ON at.tag_id = t.id").
+		Joins("LEFT JOIN `"+Article{}.TableName()+"` AS ar ON at.article_id = ar.id").
+		Where("at.`tag_id` = ? AND ar.state = ? AND ar.is_del = ?", tagID, a.State, 0).
+		Rows()
+
+	if err != nil {
 		return nil, err
+	}
+
+	defer rows.Close()
+
+	var articles []*ArticleRow
+	for rows.Next() {
+		r := &ArticleRow{}
+		if err := rows.Scan(
+			&r.ArticleID,
+			&r.ArticleTitle,
+			&r.ArticleDesc,
+			&r.CoverImageUrl,
+			&r.Content,
+			&r.TagID,
+			&r.TagName,
+		); err != nil {
+			return nil, err
+		}
+
+		articles = append(articles, r)
 	}
 
 	return articles, nil
 }
 
-func (a Article) Count(db *gorm.DB) (int, error) {
+func (a Article) CountByTagID(db *gorm.DB, tagID uint32) (int, error) {
 	var count int
+	err := db.Table(ArticleTag{}.TableName()+" AS at").
+		Joins("LEFT JOIN `"+Tag{}.TableName()+"` AS t ON at.tag_id = t.id").
+		Joins("LEFT JOIN `"+Article{}.TableName()+"` AS ar ON at.article_id = ar.id").
+		Where("at.`tag_id` = ? AND ar.state = ? AND ar.is_del = ?", tagID, a.State, 0).
+		Count(&count).Error
 
-	db.Where("state = ?", a.State)
-	if err := db.Model(&a).Where("is_del = ?", 0).Count(&count).Error; err != nil {
+	if err != nil {
 		return 0, err
 	}
 
